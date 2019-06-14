8d096f60179fbc4350674f94c62d6704c7f4f544
Joy Lin <vvlnote@hotmail.com>
2019-06-14 22:55:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  const freeFeet = 400;
+  const maxFeet = 2000;
+  const tooFar = 2500
+  let message = "";
+  if (distance <= freeFeet){
+    message = "This one is on me!";
+  } else if (distance > maxFeet && distance <= tooFar){
+    message = "I will gladly take your thirty bucks.";
+  } else {
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  return (cityName == 'NYC' ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(charm){
   // Write your code here!
+  switch (charm) {
+    case "generous":
+      return 'Thank you so much.';
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    case "thanks for everything":
+        return "Bye."
+        break;
+  }
 }
